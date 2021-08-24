f594cae5ed145667bc6d9de94d882c1e80e6edbd
Kartik Rao <kr40.git@gmail.com>
2021-08-24 11:30:46 +0530

    changed code structure

--- src/main.js
+++ src/main.js
@@ -7,9 +7,13 @@ import { switchThemeBtn, theme } from './modules/selectors';
 //* Theme Switching Functionality
 
 switchThemeBtn.addEventListener('click', function () {
-	if (theme.getAttribute('href') == 'https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css') {
-		theme.setAttribute('href', 'https://cdn.jsdelivr.net/npm/water.css@2/out/light.css');
-	} else {
-		theme.setAttribute('href', 'https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css');
-	}
+	theme.getAttribute('href') == 'https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css'
+		? theme.setAttribute('href', 'https://cdn.jsdelivr.net/npm/water.css@2/out/light.css')
+		: theme.setAttribute('href', 'https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css');
+
+	switchThemeBtn.textContent == 'Light 💡'
+		? (switchThemeBtn.textContent = 'Dark 🌑')
+		: (switchThemeBtn.textContent = 'Light 💡');
 });
+
+//* Lecture
