cecbab6477e1f9a66d590f25009c3cb28cfe200e
Kartik Rao <kr40.git@gmail.com>
2021-11-17 15:40:11 +0530

    updated packages

--- src/main.js
+++ src/main.js
@@ -15,3 +15,15 @@ switchThemeBtn.addEventListener('click', function () {
 		? (switchThemeBtn.textContent = 'Dark 🌑')
 		: (switchThemeBtn.textContent = 'Light 💡');
 });
+
+// Write a function to get the latest date
+function getDate() {
+	const date = new Date();
+	const day = date.getDate();
+	const month = date.getMonth() + 1;
+	const year = date.getFullYear();
+
+	console.log(`${day}/${month}/${year}`);
+}
+
+getDate();
